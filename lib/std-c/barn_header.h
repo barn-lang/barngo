c2c755a149bc687fc16a61c36f42b953d0f6b922
Solindek <solindeklive.biznes@gmail.com>
2023-07-31 21:49:51 +0200

    barngo: old compiler written in go

---
+++ lib/std-c/barn_header.h
@@ -0,0 +1,41 @@
+/*
+ *   Copyright (C) 2022-2023 Barn-Lang Organization
+ * 
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ * 
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef __BARN_HEADER_h__
+#define __BARN_HEADER_h__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <math.h>
+
+#if defined(__unix__)
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <unistd.h>
+    #include <sys/utsname.h>
+#elif defined(_WIN32)
+    #include <windows.h>
+#endif
+
+#define __BARN_FUNCTION__
+#define __BARN_GLOBAL_VARIABLE__
+#define __use__(x) {x;}
+
+#endif /* __BARN_HEADER_h__ */
